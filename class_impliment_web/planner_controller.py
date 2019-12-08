eeff0237971c1825bc55e7436f034317113bbf92
natnaelmesfun <natu_mes@yahoo.com>
2019-12-08 12:59:10 -0700

    deleted methods

--- class_impliment_web/planner_controller.py
+++ class_impliment_web/planner_controller.py
@@ -7,18 +7,6 @@ class User():
     def __init__(self, username):
         self.username = username
 
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.username
-
     def hash_pass(password):
         return bcrypt.hashpw(password, bcrypt.gensalt())
         
